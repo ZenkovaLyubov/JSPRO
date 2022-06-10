5bc5c1ebffd52ab6665b6e4b699d79d43ce9d96d
Lyubov <zenkova.lyuba@gmail.com>
2022-06-10 17:42:37 +0300

    hw6

--- script.js
+++ script.js
@@ -8,32 +8,101 @@ function service(url) {
 }
 
 window.onload = () => {
+
+Vue.component('alert', {
+    template:`
+    <div class="alert-window center__content">
+                      <i class="fa-solid fa-xmark close-icon-alert" @click="$emit('close')"></i>
+                      <div class="alert">
+  Ошибка сервера.
+                          
+                      </div>
+                  </div>
+    `
+  })
+
+Vue.component('base-search', {
+    model: {
+      prop: 'value',
+      event: 'change'
+    },
+    template: `
+    <div class="search">
+      <input
+        type="text" class="goods-search" placeholder="Поиск..."
+        @input="$emit('change', $event.target.value)"
+      >
+     
+      </div>
+    `
+  })
+
+Vue.component('basket', {
+  template:`
+  <div class="cart-window center__content">
+                    <i class="fa-solid fa-xmark close-icon" @click="$emit('close')"></i>
+                    <div class="cart-list ">
+
+                        <div class="goods-item">
+                            <img src="img/product.jpg" alt="photo">
+                            <h3>Название товара</h3>
+                            <p>$Цена</p>
+                        </div>
+                    </div>
+                </div>
+  `
+})
+Vue.component('custom-button', {
+    template:
+    `
+    <button class="button cart-button" type="button" @click="$emit('click')">
+    <slot></slot>
+    </button>
+    `
+  })
+Vue.component('good', {
+  props:[
+    'item'
+  ],
+  template:`
+  <div class="goods-item">
+                            <img src="https://picsum.photos/200" alt="photo">
+                            <h3>{{item.product_name}}</h3>
+                            <p>{{item.price}}</p>
+                        </div>
+  `
+  
+})
+
   const app = new Vue({
     el: '#root',
     data: {
       items: [],
       searchValue: '',
       isVisibleCart: false,
-      whileItemsEmpty: true,
-      ItemsEmpty: false,
+      isErrorAlert: false,
     },
     methods: {
-      cartVisibleToggle() {
-        if (this.isVisibleCart) {
-          this.isVisibleCart = false;
-        }
-        else {
-          this.isVisibleCart = true;
-        }
-
+      setVisibleCart() {
+        this.isVisibleCart=!this.isVisibleCart;
+      },
+      delErrorAlert(){
+        setTimeout(()=>this.isErrorAlert=false, 5000);
+      },
+      setErrorAlert(){
+        this.isErrorAlert=false;
       }
     },
     mounted() {
-      service(GOODS).then((data) => {
+        service(GOODS).then((data) => {
         this.items = data;
         this.filteredItems = data;
-        this.whileItemsEmpty = false;
         return data;
+      })
+      service(GOODS).catch(() => {
+        this.isErrorAlert = true;
+        this.delErrorAlert();
+       
       })
     },
     computed: {
